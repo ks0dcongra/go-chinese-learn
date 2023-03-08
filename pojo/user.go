6df51c8f00077270e91d8e377e9b24c9e23a54d3
ks0dcongra <ks0dcongra@gmail.com>
2023-03-08 17:21:52 +0800

    feat: revise pojo models `json:??` pattern

--- pojo/user.go
+++ pojo/user.go
@@ -1,12 +1,8 @@
 package pojo
 
 type User struct {
-	// Id int `json:"UserId"`
-	// Name string `json:"UserName"`
-	// Password string `json:"UserPassword"`
-	// Email string `json:"UserEmail"`
-	Id int 
-	Name string 
-	Password string 
-	Email string 
+	Id   int    `json:UserId`
+	Name string `json:UserName`
+	Password string `json:UserPassword`
+	Email string `json:UserEmail`
 }
